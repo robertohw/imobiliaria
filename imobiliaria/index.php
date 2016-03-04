e51116d01f7f9a4ff9d47f008b673270f3813f3b
Roberto Hiroshi <roberto.hwatanabe@gmail.com>
2016-03-04 15:35:42 -0300

    Alteração da página inicial do projeto.

--- imobiliaria/index.php
+++ imobiliaria/index.php
@@ -1,1 +1,12 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Projeto Imobiliária</title>
+</head>
+<body>
+	<h1>Projeto Imobiliária</h1>
+	<p>Alterações no projeto.</p>
+</body>
+</html>
+	
